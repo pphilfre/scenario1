e7ed2b63378ac2e79bbd05ad6fc5604cb28e28cf
_sshadow <128976479+o-sshadow@users.noreply.github.com>
2024-11-10 13:55:53 +0000

    Changed to default

--- components/tutorial/sign-up-user-steps.tsx
+++ components/tutorial/sign-up-user-steps.tsx
@@ -3,10 +3,5 @@ import { ArrowUpRight } from "lucide-react";
 
 export default function SignUpUserSteps() {
   return (
-    <ol className="flex flex-col gap-6">
-      {process.env.VERCEL_ENV === "preview" ||
-      process.env.VERCEL_ENV === "production" ? (
-      ) : null}
-    </ol>
   );
 }
